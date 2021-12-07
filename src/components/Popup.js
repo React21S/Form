01fefacc563249c81c1de527ff72d03c3df3931b
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2021-12-07 13:02:03 +0200

    props was removed

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,17 +5,17 @@ import React from 'react';
 const closePopupHandler=()=>{
     window.location.reload();
 }
-const Popup = (props) => {
+const Popup = ({firstName, lastName,phoneNumber, role, message}) => {
     return (
         <div className="popup">
             <div className="overlay">
                 <h1>Your Note</h1>
                 <div>
-                    <p>First name <span>{props.firstName}</span></p>
-                    <p>Last name <span>{props.lastName}</span></p>
-                    <p>Phone Number <span>{props.phoneNumber}</span> </p>
-                    <p>Role <span>{props.role}</span></p>
-                    <p>Message <span>{props.message}</span> </p>
+                    <p>First name <span>{firstName}</span></p>
+                    <p>Last name <span>{lastName}</span></p>
+                    <p>Phone Number <span>{phoneNumber}</span> </p>
+                    <p>Role <span>{role}</span></p>
+                    <p>Message <span>{message}</span> </p>
 
                     <button onClick={closePopupHandler}>Yes, I am Sure</button>
                     <button className="check" onClick={closePopupHandler}>Nope Don't want to post it</button>
